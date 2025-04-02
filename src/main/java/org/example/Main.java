ef7afec8808c79d2f97b230dfedd9a04e2b4eaf3
Danila Plater <danilaplyter@gmail.com>
2025-04-02 17:13:56 +0300

    Добавил переменные возраста и имя и метод вывода имени и возраста.

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int myAge = 18;
+        String myName = "Danila";
+        System.out.println("Меня зовут " + myName + ", мне " + myAge + " лет.");
+    }
+}
