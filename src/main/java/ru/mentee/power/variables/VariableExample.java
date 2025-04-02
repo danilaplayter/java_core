04a79f73f5d1c9e420cf28f00955722d7bcf8e4e
Danila Plater <danilaplyter@gmail.com>
2025-04-02 17:27:49 +0300

    Добавил класс VariableExample.

---
+++ src/main/java/ru/mentee/power/variables/VariableExample.java
@@ -0,0 +1,17 @@
+package ru.mentee.power.variables;
+
+public class VariableExample {
+    public static void main(String[] args) {
+        // Объявляем переменную и даем ей значение
+        int age = 25;
+
+        // Используем переменную
+        System.out.println("Возраст: " + age);
+
+        // Меняем значение переменной
+        age = 26;
+
+        // Снова используем переменную, уже с новым значением
+        System.out.println("Новый возраст: " + age);
+    }
+}
