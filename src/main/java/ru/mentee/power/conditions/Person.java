052213bd8e3b8c0bccc77dae4e4c2e47ed22b2fa
Danila Plater <danilaplyter@gmail.com>
2025-04-04 09:46:37 +0300

    MP-22: Выполнил.

---
+++ src/main/java/ru/mentee/power/conditions/Person.java
@@ -0,0 +1,29 @@
+package ru.mentee.power.conditions;
+
+public class Person {
+    private int age;
+    private boolean licence;
+
+    public Person() {
+    }
+
+    public boolean ableToRent(){
+        return age >= 18;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isLicence() {
+        return licence;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setLicence(boolean licence) {
+        this.licence = licence;
+    }
+}
