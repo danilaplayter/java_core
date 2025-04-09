22e1efaa1941714348dc793a4d8d2788ecc89623
Danila Plater <danilaplyter@gmail.com>
2025-04-09 17:34:25 +0300

    MP-42: Выполнил.

---
+++ src/main/java/ru/mentee/power/methods/library/Book.java
@@ -0,0 +1,40 @@
+package ru.mentee.power.methods.library;
+
+public class Book {
+    private String title;
+    private String author;
+    private int year;
+    private boolean available;
+
+    Book(String title, String author, int year) {
+        this.title = title;
+        this.author = author;
+        this.year = year;
+        this.available = true;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public boolean isAvailable(){
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    @Override
+    public String toString() {
+        return title + " " + author + " " + Integer.toString(year) + " " + Boolean.toString(available);
+    }
+}
